7bf243b628934d9010f0f58cd490e9baa504a9d6
Michael Hopkins <michael@appitventures.com>
2014-06-13 11:21:52 -0600

    removed invalid api key. oops

--- app/config/packages/bugsnag/bugsnag-laravel/config.php
+++ app/config/packages/bugsnag/bugsnag-laravel/config.php
@@ -3,7 +3,7 @@
      * Set your Bugsnag API Key.
      * You can find your API Key on your Bugsnag dashboard.
      */
-    'api_key' => '9c83056b119aac982ed2b762184702ca',
+    'api_key' => '',
 
     /**
      * Set which release stages should send notifications to Bugsnag
