ba5eac47b8e0197d89242588b8f11e2c543e57fc
Michael Hopkins <michael@appitventures.com>
2014-08-17 04:11:13 -0600

    adding seed files back in

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -1,17 +1,24 @@
 <?php
 
+use Illuminate\Database\Seeder;
 class DatabaseSeeder extends Seeder {
 
-	/**
-	 * Run the database seeds.
-	 *
-	 * @return void
-	 */
-	public function run()
-	{
-		Eloquent::unguard();
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Eloquent::unguard();
 
-		// $this->call('UserTableSeeder');
-	}
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
 
+
+        $this->call('SentryGroupSeeder');
+        $this->call('TestUsersSeeder');
+        $this->call('UsersTableSeeder');
+
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+    }
 }
